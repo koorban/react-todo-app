563ef10699006e4b5153addce139c3ea617f2621
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-11-04 14:25:45 -0500

    removing boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import TodoForm from './TodoForm';
+import UncompletedList from './UncompletedList';
+import CompletedList from './CompletedList';
+import ButtonDeleteAll from './ButtonDeleteAll';
+import ButtonDeleteUncompletedList from './ButtonDeleteUncompletedList';
+import ButtonDeleteCompletedList from './ButtonDeleteCompletedList';
+import AlertTodoCompleted from './AlertTodoCompleted';
+import AlertDeleteAll from './AlertDeleteAll';
+import AlertDeleteUncompletedList from './AlertDeleteUncompletedList';
+import AlertDeleteCompletedList from './AlertDeleteCompletedList';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      <Row className=" justify-content-center mb-5">
+        <Col xs={{ offset: 3 }}md={{ offset: 4 }}>
+          <TodoForm />
+        </Col>
+      </Row>
+      <Row className="mb-5">
+        <Col>
+          <ButtonDeleteAll />
+        </Col>
+        <Col>
+          <ButtonDeleteUncompletedList />
+        </Col>
+        <Col>
+          <ButtonDeleteCompletedList />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <AlertTodoCompleted />
+          <AlertDeleteAll />
+          <AlertDeleteUncompletedList />
+          <AlertDeleteCompletedList />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <UncompletedList />
+        </Col>
+        <Col>
+          <CompletedList />
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
