4f272af2a33aae78b8d7ae2da1472bac68f3c0ad
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-11-04 14:28:04 -0500

    add alert for deleting completed list

---
+++ src/AlertDeleteCompletedList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { TodoContext } from './ContextTodo';
+import Alert from 'react-bootstrap/Alert';
+
+export default function ClearCompletedListAlert() {
+    const [show, setShow] = useState(false);
+
+    const { isButtonDeleteCompletedClicked , setIsButtonDeleteCompletedClicked } = useContext(TodoContext);
+
+    const handleClose = () => {
+        setShow(false);
+        setIsButtonDeleteCompletedClicked(false);
+    };
+
+    useEffect(() => {
+        if (isButtonDeleteCompletedClicked === true && show === false) return setShow(true);
+    }, [isButtonDeleteCompletedClicked]);
+
+    return (
+        <>
+            { show && isButtonDeleteCompletedClicked &&
+                <Alert variant='success' onClose={() => handleClose()} dismissible>
+                    <Alert.Heading> Completed List Cleared!</Alert.Heading>
+                    <p>
+                        Good Job!
+                    </p>
+                </Alert>
+            }
+        </>
+    )
+}
