dfbb4a67f81540ec69c0d1334f0b68cc6e4f10dd
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-11-04 14:29:05 -0500

    add alert for completed todo

---
+++ src/AlertTodoCompleted.js
@@ -0,0 +1,31 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { TodoContext } from './ContextTodo';
+import Alert from 'react-bootstrap/Alert';
+
+export default function TodoAlert() {
+    const [show, setShow] = useState(false);
+
+    const { todoClicked, setTodoClicked } = useContext(TodoContext);
+    
+    const handleClose = () => {
+        setShow(false);
+        setTodoClicked(false);
+    };
+
+    useEffect(() => {
+        if (todoClicked === true && show === false) return setShow(true);
+    }, [todoClicked]);
+
+    return (
+        <>
+            { show && todoClicked &&
+                <Alert variant='success' onClose={() => handleClose()} dismissible>
+                    <Alert.Heading>Todo Completed!</Alert.Heading>
+                    <p>
+                        Congratulations!
+                    </p>
+                </Alert>
+            }
+        </>
+    )
+};
